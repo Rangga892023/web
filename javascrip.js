5fe63f9a62796f25e8677a963461aa3e3cd1a82f
Rangga892023 <franggae@gmail.com>
2024-07-01 21:46:49 +0700

    Add files via upload

---
+++ javascrip.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav ul');
+
+    burger.addEventListener('click', function() {
+        nav.classList.toggle('nav-active');
+
+        burger.classList.toggle('toggle');
+    });
+});
